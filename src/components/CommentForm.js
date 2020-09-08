a402596160d4448d8d9587d73144fd43d044b184
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-09-08 14:28:01 +0200

    css

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -24,59 +24,65 @@ function CommentForm() {
   };
 
   return (
-    <div className="comment-view-grid comments-card">
-      <form id="comment-form" className="comment-form" onSubmit={handleSubmit}>
-        <p className="comment-title">Leave a Reply</p>
-        <hr className="comments-horizontal-line" />
-        <p className="comment-notes">
-          <span id="email-notes">
-            Your email adress will not be published. Required fields are marked
-          </span>
-          <span className="required"> *</span>
-        </p>
+    <form
+      id="comment-form"
+      className="comment-form comment-view-grid comments-card"
+      onSubmit={handleSubmit}
+    >
+      <p className="comment-title">Leave a Reply</p>
+      <hr className="comment-title-bottom-line" />
+      <p className="comment-notes">
+        <span id="email-notes">
+          Your email adress will not be published. Required fields are marked
+        </span>
+        <span className="required"> *</span>
+      </p>
 
-        <p className="comment-form-comment">
-          <label htmlFor="comment">Comment</label>
-          <textarea
-            id="body"
-            name="body"
-            value={data.body}
-            onChange={handleInputChange}
-            cols="45"
-            rows="8"
-          />
-        </p>
+      <p className="comment-form-comment">
+        <label htmlFor="body"></label>
+        <textarea
+          id="body"
+          name="body"
+          value={data.body}
+          onChange={handleInputChange}
+          cols="45"
+          rows="8"
+          placeholder="Write comment..."
+        />
+      </p>
 
-        <p className="comment-form-author">
-          <label htmlFor="name">
-            Name<span className="required"> *</span>
-          </label>
-          <input
-            id="name"
-            name="name"
-            value={data.name}
-            onChange={handleInputChange}
-          />
-        </p>
+      <p className="comment-form-author">
+        <label htmlFor="name">
+          Name<span className="required"> *</span>
+        </label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={data.name}
+          onChange={handleInputChange}
+          placeholder="Your name..."
+        />
+      </p>
 
-        <p className="comment-form-email">
-          <label htmlFor="email">
-            Email Address<span className="required"> *</span>
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={data.email}
-            onChange={handleInputChange}
-          />
-        </p>
+      <p className="comment-form-email">
+        <label htmlFor="email">
+          Email Address<span className="required"> *</span>
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={data.email}
+          onChange={handleInputChange}
+          placeholder="Your email..."
+        />
+      </p>
 
-        <p className="form-submit">
-          <input id="submit" type="submit" value="Post Comment"></input>
-        </p>
-      </form>
-    </div>
+      <p className="form-submit">
+        <input id="submit" type="submit" value="Post Comment"></input>
+      </p>
+    </form>
   );
 }
 
--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -5,10 +5,10 @@ import CommentView from "./CommentView";
 function CommentsList(props) {
   return (
     <ol className="comment-view-grid comments-card">
-      <p className="comments-view-count">{props.comments.length} comments</p>
+      <p className="comment-title">{props.comments.length} comments</p>
       {props.comments.length > 0 && (
         <>
-          <hr className="comments-view-conunt-bottom-line" />
+          <hr className="comment-title-bottom-line" />
           {props.comments.map((comment, index) => (
             <CommentView key={index} data={comment} />
           ))}
